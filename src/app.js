e08da1c11cd381224a862db4c70a73430109a94d
sumpierrezf <viqui09umpierrez@gmail.com>
2023-01-05 15:25:00 +0000

    agregando funcionalidades

--- src/app.js
+++ src/app.js
@@ -24,30 +24,56 @@ window.onload = function() {
     "J",
     "As"
   ];
-  let indexPalos = Math.floor(Math.random() * palos.length);
-  let indexNumeros = Math.floor(Math.random() * numeros.length);
 
-  let color =
-    palos[indexPalos] == "♦" || palos[indexPalos] == "♥" ? "red" : "black";
+  //let color = palos[indexPalos] == "♦" || palos[indexPalos] == "♥" ? "red" : "black";
 
-  console.log(palos[indexPalos]);
-  let top = document.getElementById("top");
-  let bottom = document.getElementById("bottom");
-  let number = document.getElementById("number");
-  top.innerHTML = palos[indexPalos];
-  document.getElementById("top").style.color = color;
-  document.getElementById("bottom").style.color = color;
-  document.getElementById("number").style.color = color;
-  bottom.innerHTML = palos[indexPalos];
-  number.innerHTML = numeros[indexNumeros];
+  /*console.log(palos[indexPalos]);
+                                              let top = document.getElementById("top");
+                                              let bottom = document.getElementById("bottom");
+                                              let number = document.getElementById("number");
+                                              top.innerHTML = palos[indexPalos];
+                                              document.getElementById("top").style.color = color;
+                                              document.getElementById("bottom").style.color = color;
+                                              document.getElementById("number").style.color = color;
+                                              bottom.innerHTML = palos[indexPalos];
+                                              number.innerHTML = numeros[indexNumeros];*/
+  //declaramos un array que tendra todas las cartas
+  let todasCartas = [];
 
   for (let i = 1; i <= 50; i++) {
-    document.body.innerHTML += `<div class="card" style="width: 18rem;">
+    let indexPalos = Math.floor(Math.random() * palos.length);
+    let indexNumeros = Math.floor(Math.random() * numeros.length);
+    //aca obtengo el palo
+    console.log(palos[indexPalos]);
+    //aca obtengo el numero
+    console.log(numeros[indexNumeros]);
+    //generamos la variable carta que es el array que tendra el conjunto de datos; palos y numeros
+    let carta = [palos[indexPalos], numeros[indexNumeros]];
+    console.log(carta);
+    //llamamos al array y con push ingresamos cada conjunto de datos que se genera con iteracion
+    todasCartas.push(carta);
+    console.log(todasCartas);
+
+    if (palos[indexPalos] === "♥" || palos[indexPalos] === "♦") {
+      document.body.innerHTML += `<div class="card" style="width: 18rem;">
     <ul class="list-group list-group-flush">
-      <li class="list-group-item" id="top">${palos[indexPalos]}</li>
-      <li class="list-group-item" id="number">${numeros[indexNumeros]}</li>
-      <li class="list-group-item" id="bottom">${palos[indexPalos]}</li>
+      <li class="list-group-item text-danger" id="top">${palos[indexPalos]}</li>
+      <li class="list-group-item text-danger" id="number">${numeros[indexNumeros]}</li>
+      <li class="list-group-item text-danger" id="bottom">${palos[indexPalos]}</li>
     </ul>
   </div>`;
+    } else {
+      document.body.innerHTML += `<div class="card" style="width: 18rem;">
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item" id="top">${palos[indexPalos]}</li>
+        <li class="list-group-item" id="number">${numeros[indexNumeros]}</li>
+        <li class="list-group-item" id="bottom">${palos[indexPalos]}</li>
+      </ul>
+    </div>`;
+    }
   }
+
+  const bubbleSort = todasCartas => {
+    console.log(bubbleSort(todasCartas));
+  };
 };
